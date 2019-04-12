b581e017b566df18f89cfc6bd6b38cede145e317
M.Harazin <mh757@kent.ac.uk>
2019-04-12 21:18:51 +0100

    [3:13] - Included nested JSX's.

--- client/main.js
+++ client/main.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
 Meteor.startup(function () {
+	let title = 'Score Keep';
   let name = 'Mike';
-  let jsx = <p>Hello {name}!</p>;
+  let jsx = (
+		<div>
+			<h1>{title}</h1>
+			<p>Hello {name}!</p>
+			<p>This is my second paragraph.</p>
+		</div>
+	);
   ReactDOM.render(jsx, document.getElementById('app'));
 });
