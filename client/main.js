6daeeb3fbc25316fe35a860a1941bd5ac716a4da
M.Harazin <mh757@kent.ac.uk>
2019-04-12 21:39:02 +0100

    [3::14] - Added a list of players.

--- client/main.js
+++ client/main.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const players =
+[{
+	_id: '1',
+	name: 'Tom',
+	score: 99
+	},
+	{
+	_id: '2',
+	name: 'Max',
+	score: 13
+	},
+	{
+	_id: '3',
+	name: 'Simon',
+	score: 0
+}];
+
+const renderPlayers = function (playersList) {
+
+	return playersList.map(function (player) {
+		return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
+	});
+};
+
 Meteor.startup(function () {
 	let title = 'Score Keep';
   let name = 'Mike';
@@ -10,6 +34,7 @@ Meteor.startup(function () {
 			<h1>{title}</h1>
 			<p>Hello {name}!</p>
 			<p>This is my second paragraph.</p>
+			{renderPlayers(players)}
 		</div>
 	);
   ReactDOM.render(jsx, document.getElementById('app'));
