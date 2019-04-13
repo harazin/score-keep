f85409a7ad1acd8a4af8a25b73cda501c3ae4e29
M.Harazin <mh757@kent.ac.uk>
2019-04-13 09:33:51 +0100

    [3:18] - Added sync between MongoDB and MiniMongo.

--- client/main.js
+++ client/main.js
@@ -3,44 +3,34 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 import {Tracker} from 'meteor/tracker';
-Tracker.autorun(function () {
-	console.log('Players List: ', Players.find().fetch());
-});
-
-const players =
-[{
-	_id: '1',
-	name: 'Tom',
-	score: 99
-	},
-	{
-	_id: '2',
-	name: 'Max',
-	score: 13
-	},
-	{
-	_id: '3',
-	name: 'Simon',
-	score: 0
-}];
 
 const renderPlayers = function (playersList) {
-
 	return playersList.map(function (player) {
 		return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
 	});
 };
 
 Meteor.startup(function () {
-	let title = 'Score Keep';
-  let name = 'Mike';
-  let jsx = (
-		<div>
-			<h1>{title}</h1>
-			<p>Hello {name}!</p>
-			<p>This is my second paragraph.</p>
-			{renderPlayers(players)}
-		</div>
-	);
-  ReactDOM.render(jsx, document.getElementById('app'));
+	Tracker.autorun(function () {
+		let players = Players.find().fetch();
+
+		let title = 'Score Keep';
+		let name = 'Mike';
+		let jsx = (
+			<div>
+				<h1>{title}</h1>
+				<p>Hello {name}!</p>
+				<p>This is my second paragraph.</p>
+				{renderPlayers(players)}
+			</div>
+		);
+		ReactDOM.render(jsx, document.getElementById('app'));
+	});
+
+	setTimeout(function () {
+		Players.insert({
+			name: 'Matt',
+			score: 12
+		});
+	}, 1000);
 });
