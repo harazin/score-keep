be41327b18fd74ebb197cc1c9f9e6dcde6d662dd
M.Harazin <mh757@kent.ac.uk>
2019-04-13 17:13:34 +0100

    [4:35] - Refactored app rendering using App Component.

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 export default class TitleBar extends React.Component {
+	renderSubtitle() {
+		if(this.props.subtitle) {
+			return <h2>{this.props.subtitle}</h2>
+		}
+	}
 	render() {
 		return (
 			<div>
 				<h1>{this.props.title}</h1>
-				<h2>{this.props.subtitle}</h2>
+				{this.renderSubtitle()}
 			</div>
-		)
+		);
 	}
 }
 
 TitleBar.propTypes = {
-	title: PropTypes.string,
+	title: PropTypes.string.isRequired,
 	subtitle: PropTypes.string
 };
 
