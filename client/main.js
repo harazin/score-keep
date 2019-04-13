bd49f953b2e45c2cf9097b69931f2baee75cf078
M.Harazin <mh757@kent.ac.uk>
2019-04-13 09:22:22 +0100

    [3:17] - Included Tracker for MongoDB.

--- client/main.js
+++ client/main.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players';
+import {Tracker} from 'meteor/tracker';
+Tracker.autorun(function () {
+	console.log('Players List: ', Players.find().fetch());
+});
 
 const players =
 [{
