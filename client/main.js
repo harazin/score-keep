d4ffb6cf380ccecd13ae7d10a34307e6ce20b894
M.Harazin <mh757@kent.ac.uk>
2019-04-13 10:42:18 +0100

    [3:23] - Added increment and decrement buttons to individual players.

--- client/main.js
+++ client/main.js
@@ -8,6 +8,8 @@ const renderPlayers = (playersList) => {
 	return playersList.map((player) => {
 		return (
 			<p key={player._id}>{player.name} has {player.score} point(s).
+			<button onClick={() => Players.update({_id: player._id}, {$inc: {score: 1}})}>+1</button>
+			<button onClick={() => Players.update({_id: player._id}, {$inc: {score: -1}})}>-1</button>
 			<button onClick={() => Players.remove(player._id)}>X</button>
 			</p>
 		);
