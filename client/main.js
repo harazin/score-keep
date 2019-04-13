e6a9fbaf46e37b2c8dd3376bab78c48ec5651118
M.Harazin <mh757@kent.ac.uk>
2019-04-13 16:39:23 +0100

    [4:32] - Refactored Player into Component.

--- client/main.js
+++ client/main.js
@@ -6,16 +6,11 @@ import {Tracker} from 'meteor/tracker';
 
 import TitleBar from './../imports/ui/TitleBar';
 import AddPlayer from './../imports/ui/AddPlayer';
+import Player from './../imports/ui/Player';
 
 const renderPlayers = (playersList) => {
 	return playersList.map((player) => {
-		return (
-			<p key={player._id}>{player.name} has {player.score} point(s).
-			<button onClick={() => Players.update({_id: player._id}, {$inc: {score: 1}})}>+1</button>
-			<button onClick={() => Players.update({_id: player._id}, {$inc: {score: -1}})}>-1</button>
-			<button onClick={() => Players.remove(player._id)}>X</button>
-			</p>
-		);
+		return <Player key={player._id} player={player}/>; 
 	});
 };
 
