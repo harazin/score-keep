af1677a6ad9b188f5443f9146e6d4d351a904b4e
M.Harazin <mh757@kent.ac.uk>
2019-04-14 17:09:47 +0100

    [5:46] - Added numeral package and calculated player positions.

--- server/main.js
+++ server/main.js
@@ -4,3 +4,30 @@ import {Players} from './../imports/api/players';
 Meteor.startup(() => {
 
 });
+
+let user = {
+	name: 'Andrew',
+	location: 'London'
+};
+
+let person = {
+	...user,
+	age: 21
+};
+
+let house = {
+	bedrooms: 2,
+	bathrooms: 1.5
+};
+
+let yearBuilt = 1995;
+
+let building = {
+	...house,
+	bedrooms: 3,
+	yearBuilt,
+	flooring: 'Carpet'
+};
+
+
+console.log(building);
